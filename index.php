614e215d00ca56ad1fd8c7b70ba5ed610628e2ef
Kunal <kunmishra2599@gmail.com>
2016-11-05 22:58:38 +0530

    made css changes

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   		<title>WorkTeen</title>
   		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
 		<LINK href="assets/stylesheets/pgName.css" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
 		<link href="assets/stylesheets/index.css" rel="stylesheet">
 	</head>
 	<body>
@@ -17,12 +18,19 @@
 			</div>
 			<div class="pure-u-2-3"></div> <!--Spacers-->
 			<div class="pure-u-1-3 header">
-				<h2 class="big-header">LEARN.</h2>
-				<h2 class="big-header">CREATE.</h2>
-				<h2 class="big-header">SERVE.</h2>
+				<h2 class="big-header">LEARN. CREATE. SERVE.</h2>
 			</div>
 			<div class="pure-u-2-3"></div> <!--Spacers-->
+			<div class="pure-u-1-5 header">
+				<div class="myotherButton">Post opportunity ></div>
+			</div>
+			<div class="pure-u-4-5"></div> <!--Spacers-->
 		</div>
+
+
+
+
+
 		<div class="pure-g middle">
 			<div class="pure-u-1-3 header">
 				<h2>JOIN WORKTEEN</h2>
@@ -34,16 +42,15 @@
 				<h2 class="big-header">SERVE.</h2>
 			</div>
 			<div class="pure-u-2-3"></div> <!--Spacers-->
+			
 		</div>
 		<div class="pure-g end">
 			<div class="pure-u-1-3 header">
 				<h2>JOIN WORKTEEN</h2>
 			</div>
 			<div class="pure-u-2-3"></div> <!--Spacers-->
-			<div class="pure-u-1-3 header">
-				<h2 class="big-header">LEARN.</h2>
-				<h2 class="big-header">CREATE.</h2>
-				<h2 class="big-header">SERVE.</h2>
+			<div class="pure-u-1 header">
+				<h2 class="big-header">LEARN. CREATE. SERVE.</div>
 			</div>
 			<div class="pure-u-2-3"></div> <!--Spacers-->
 		</div>
