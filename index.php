a0e8f91c178058c07ea8d0ca15fa06a50c4a49b6
Kunal <kunmishra2599@gmail.com>
2016-11-05 15:29:04 +0530

    Made three sections, added BG image, other stuff too

--- index.php
+++ index.php
@@ -7,8 +7,46 @@
   		<title>WorkTeen</title>
   		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
 		<LINK href="assets/stylesheets/pgName.css" rel="stylesheet">
+		<link href="assets/stylesheets/index.css" rel="stylesheet">
 	</head>
 	<body>
 		<?php include('assets/navbar.html'); ?>
+		<div class="pure-g top">
+			<div class="pure-u-1-3 header">
+				<h2>JOIN WORKTEEN</h2>
+			</div>
+			<div class="pure-u-2-3"></div> <!--Spacers-->
+			<div class="pure-u-1-3 header">
+				<h2 class="big-header">LEARN.</h2>
+				<h2 class="big-header">CREATE.</h2>
+				<h2 class="big-header">SERVE.</h2>
+			</div>
+			<div class="pure-u-2-3"></div> <!--Spacers-->
+		</div>
+		<div class="pure-g middle">
+			<div class="pure-u-1-3 header">
+				<h2>JOIN WORKTEEN</h2>
+			</div>
+			<div class="pure-u-2-3"></div> <!--Spacers-->
+			<div class="pure-u-1-3 header">
+				<h2 class="big-header">LEARN.</h2>
+				<h2 class="big-header">CREATE.</h2>
+				<h2 class="big-header">SERVE.</h2>
+			</div>
+			<div class="pure-u-2-3"></div> <!--Spacers-->
+		</div>
+		<div class="pure-g end">
+			<div class="pure-u-1-3 header">
+				<h2>JOIN WORKTEEN</h2>
+			</div>
+			<div class="pure-u-2-3"></div> <!--Spacers-->
+			<div class="pure-u-1-3 header">
+				<h2 class="big-header">LEARN.</h2>
+				<h2 class="big-header">CREATE.</h2>
+				<h2 class="big-header">SERVE.</h2>
+			</div>
+			<div class="pure-u-2-3"></div> <!--Spacers-->
+		</div>
+
 	</body>
 </html>
