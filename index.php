f4809fab14a66e05d31cf8c0ce20371c70b6be4a
Kunal <kunmishra2599@gmail.com>
2016-11-05 14:12:31 +0530

    Added files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<LINK href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  		<title>WorkTeen</title>
+  		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+		<LINK href="assets/stylesheets/pgName.css" rel="stylesheet">
+	</head>
+	<body>
+		<?php include('assets/navbar.html'); ?>
+	</body>
+</html>
